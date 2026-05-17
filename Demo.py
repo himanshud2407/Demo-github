50e2e4eabe410ba85dc5b772f6e6233ea83f763e
Himanshud2407 <himanshud2407@gmail.com>
2026-05-17 10:36:24 +0530

    Demo.py file add

---
+++ Demo.py
@@ -0,0 +1,4 @@
+print("Hello world")
+print("This is a demo file.")
+print("This file is used to demonstrate how to use GitHub.")
+print("This file is also used to demonstrate how to use GitHub Desktop.")
